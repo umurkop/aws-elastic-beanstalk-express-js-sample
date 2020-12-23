0770641b312a106883e3a66b3d84efb3339c4d25
umurkop <aumur.kop@gmail.com>
2020-12-23 22:25:51 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yatma vakti'));
+app.get('/', (req, res) => res.send('Time to study!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
